4c89ef20cf619613266f16ec4a912ccb8d91d5fa
Deepak Singh <singhdeepak5219@gmail.com>
2023-08-19 23:21:47 +0530

    attendance model added

---
+++ models/attendance-model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+var AttendanceSchema = new Schema({
+
+    employeeID: {type: Schema.Types.ObjectId, ref: 'User', required: true},
+    year: {type: Number, required: true},
+    month: {type: Number, required: true},
+    date: {type: Number, required: true},
+    present: {type: Boolean, required: true},
+
+});
+
+
+module.exports = mongoose.model('Attendance', AttendanceSchema);
