9538a60ab0340cb0702e5e2a22007165dfb18880
Deepak Singh <singhdeepak5219@gmail.com>
2023-08-20 10:57:14 +0530

    attendance route added

--- routes/employee-route.js
+++ routes/employee-route.js
@@ -3,7 +3,8 @@ const asyncMiddleware = require('../middlewares/async-middleware');
 const userController = require('../controllers/user-controller');
 const upload = require('../services/file-upload-service');
 
-router.patch('/user',upload.single('profile'),asyncMiddleware(userController.updateUser));          // Update Self Account
+router.patch('/user',upload.single('profile'),asyncMiddleware(userController.updateUser));  // Update Self Account
+router.post('/mark-employee-attendance',asyncMiddleware(userController.markEmployeeAttendance));
 
 
 module.exports = router;
