010f34496e9655c5d23ed59378de1949ea8443b6
Deepak Singh <singhdeepak5219@gmail.com>
2023-08-20 23:57:35 +0530

    attendance service added

--- services/attendance-service.js
+++ services/attendance-service.js
@@ -3,6 +3,7 @@ const Attendance = require('../models/attendance-model');
 class AttendanceService{
     markAttendance = async data => Attendance.create(data);
     findAttendance = async (data) => Attendance.findOne(data);
+    findAllAttendance = async (data) => Attendance.find(data);
 }
 
 module.exports = new AttendanceService();
