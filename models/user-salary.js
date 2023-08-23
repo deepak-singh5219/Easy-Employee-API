fc6de357ee50472bfa103929868bb3aacc1cfc80
Deepak Singh <singhdeepak5219@gmail.com>
2023-08-23 23:56:23 +0530

    salary schema added

---
+++ models/user-salary.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSalarySchema = new Schema({
+    employeeID: {type: Schema.Types.ObjectId, ref: 'User', required: true},
+    salary: {type: Number, default: 0},
+    bonus: {type: Number, default: 0},
+    reasonForBonus: {type: String, default: 'N/A'},
+});
+
+
+module.exports = mongoose.model('UserSalary', UserSalarySchema);
