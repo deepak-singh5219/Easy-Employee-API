af2a53460a4f0e55ae1b74edd9ddac6398293f6d
Deepak Singh <singhdeepak5219@gmail.com>
2023-11-23 18:18:00 +0530

    debug test 1

--- services/token-service.js
+++ services/token-service.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const TokenModel = require('../models/token-model');
-const accessTokenSecretKey = process.env.ACCESS_TOKEN_SECRET_KEY || '9d50ebd75b8eb1b6d754f95695b04aff86562f9a2dc9992ef574d82eceae20207a94a878c32563c602837c72ba9475867744c524b2cc90f2d2311bda0188cad7'
-const refreshTokenSecretKey = process.env.REFRESH_TOKEN_SECRET_KEY || '16ed5545a82006b2ce13b8c5a7f899d9c2eb72ef45872ce356039d9da53ef4988a9a77aaae9729d3094588aa41ab0ab9b34115eaa9531d775b4019fb2005385d'
+const accessTokenSecretKey = process.env.ACCESS_TOKEN_SECRET_KEY;
+const refreshTokenSecretKey = process.env.REFRESH_TOKEN_SECRET_KEY;
 class TokenService {
 
     generateToken = (payload) =>
